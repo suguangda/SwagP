21f8dfb9263c049b0c1f70b6fe73eb353d45b879
Gary Sun <garysun@v1020-wn-56-242.campus-dynamic.uwaterloo.ca>
2016-11-05 16:56:37 -0400

    stat

--- Statistics.cpp
+++ Statistics.cpp
@@ -11,6 +11,8 @@ int convertToIntArray(int argc, char* argv[],int array[]){
     int j=0;
     int count=0;
     int number=0;
+    int negCheck=0;
+    int numCheck=0;
 
     while(*(argv[i]+count)!='\0')
     {
@@ -19,37 +21,55 @@ int convertToIntArray(int argc, char* argv[],int array[]){
 
     count--;
 
-    while(*(argv[i]+j)!='\0')
-    {
-      switch(*(argv[i]+j))
-      {
-        case '0': number += 0*pow(10,count);
-        break;
-        case '1': number += 1*pow(10,count);
-        break;
-        case '2': number += 2*pow(10,count);
-        break;
-        case '3': number += 3*pow(10,count);
-        break;
-        case '4': number += 4*pow(10,count);
-        break;
-        case '5': number += 5*pow(10,count);
-        break;
-        case '6': number += 6*pow(10,count);
-        break;
-        case '7': number += 7*pow(10,count);
-        break;
-        case '8': number += 8*pow(10,count);
-        break;
-        case '9': number += 9*pow(10,count);
-        break;
-        default: goto OUT;
+    while(*(argv[i]+j)!='\0'){
+        switch(*(argv[i]+j)){
+            case '-': negCheck=1;
+            break;
+            case '0': number += 0*pow(10,count);
+
+            break;
+            case '1': number += 1*pow(10,count);
+                      numCheck +=1;
+            break;
+            case '2': number += 2*pow(10,count);
+                      numCheck +=2;
+            break;
+            case '3': number += 3*pow(10,count);
+                      numCheck +=3;
+            break;
+            case '4': number += 4*pow(10,count);
+                      numCheck +=4;
+            break;
+            case '5': number += 5*pow(10,count);
+                      numCheck +=5;
+            break;
+            case '6': number += 6*pow(10,count);
+                      numCheck +=6;
+            break;
+            case '7': number += 7*pow(10,count);
+                      numCheck +=7;
+            break;
+            case '8': number += 8*pow(10,count);
+                      numCheck +=8;
+            break;
+            case '9': number += 9*pow(10,count);
+                      numCheck +=9;
+            break;
+            default: goto OUT;
 
       }
       count--;
       j++;
     }
-      array[i-1]=number;
+      if (numCheck>46){
+          goto OUT;
+      }
+      else if (negCheck==0){
+          array[i-1]=number;
+      }
+      else{
+          array[i-1]=-1*number;
+      }
       i++;
 
 
@@ -78,7 +98,7 @@ int findMin(int array[],int numElements){
      return tempMin;
  }
 
-float average(int array[],float numElements){
+float average(int array[],int numElements){
     float sum=0;
     for (int i=0;i<numElements;i++){
         sum+=array[i];
@@ -87,10 +107,12 @@ float average(int array[],float numElements){
 }
 
 int main(int argc, char* argv[]){
+
     int temp0=argc;
     char** temp1=argv;
     int array[20]={0};
     convertToIntArray(temp0,temp1,array);
+
     if (argc<=20){
         if(convertToIntArray(temp0,temp1,array)<0){
             cerr <<"Error :Argument "<<convertToIntArray(temp0,temp1,array)*-1<<" is not an integer.";
